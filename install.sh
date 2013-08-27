b340742962df0a81f08237ba0555215fc104407d
Barney Hanlon <bhanlon@inviqa.com>
2013-08-27 17:14:23 +0100

    t

--- install.sh
+++ install.sh
@@ -21,6 +21,14 @@ function clrstdin () {
   return 0;
 }
 
+if readlink /proc/$$/fd/0 | grep -q "^pipe:"; then
+  echo "Pipe input:"
+elif file $( readlink /proc/$$/fd/0 ) | grep -q "character special"; then
+  echo "Standard input:"
+else
+  echo "File input:"
+fi
+
 ECHO=_echo
 REPO_BRANCH=${1:-"master"}
 
