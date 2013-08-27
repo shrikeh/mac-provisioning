1c10b5aa1da0ff8729535329a37f8e58bf70c057
Barney Hanlon <bhanlon@inviqa.com>
2013-08-27 16:18:26 +0100

    t

--- install.sh
+++ install.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 # Colour _echo to make it easier to read
 function _echo () {
-  blue=$(tput setaf 4)
-  normal=$(tput sgr0)
-  \printf "\n%40s\n\n" "${blue} ${1} ${normal}";
-  return 0;
+  {
+    blue=$(tput setaf 4)
+    normal=$(tput sgr0)
+    \printf "\n%40s\n\n" "${blue} ${1} ${normal}";
+    return 0;
+  } <&-
 }
 
 function command_exists () {
     \command -v ${1} > /dev/null 2>&1 || {
       return 1;
     }
+}
+
+function clrstdin () {
+  \read -d '' -t 0 -n 10000
 }
 
 ECHO=_echo
@@ -25,15 +31,13 @@ OSX_GCC_TARGET=${4:-"/"}
 
 # Check we aren't runniing as root as this will mess everything up"
 if [[ $EUID = 0 ]]; then
-   $ECHO "This script must not be run as root" 1>&2;
+   ${ECHO} "This script must not be run as root" 1>&2;
    exit 1;
 fi
-
-while \read -r -n 1 -t 1; do
-  break
-done
-
-$ECHO "This script requires sudo privileges. You will be prompted for your password. Do you wish to continue?";
+{
+${ECHO} "This script requires sudo privileges. You will be prompted for your password. Do you wish to continue?";
+} <&-
+clrstdin
 while \read -r -n 1 -s answer; do
   if [[ ${answer} = [YyNn] ]]; then
     if [[ ! ${answer} =~ ^[Yy]$ ]]; then
@@ -49,7 +53,8 @@ $ECHO "Making all hidden files visible in Finder"
 defaults write com.apple.Finder AppleShowAllFiles YES
 
 
-$ECHO "Do you wish to generate a new SSH key?";
+${ECHO} "Do you wish to generate a new SSH key?";
+clrstdin
 while \read  -r -n 1 -s answer; do
   if [[ ${answer} = [YyNn] ]]; then
     if [[ ${answer} =~ ^[Yy]$ ]]; then
@@ -60,116 +65,127 @@ while \read  -r -n 1 -s answer; do
   fi
 done
 
+
 # Is this running remotely (i.e. with curl and piped to sh) or has this been downloaded?
 # If not downloaded, we need to get the rest of the archive
 # @todo: check locally if we are in a folder containing the CLT
+{
+  if [[ ! -e ${ZIP_TARGET} ]]; then
+    ${ECHO} "Downloading Mac OSX Command Tools for Mountain Lion"
+    \curl -fsSL -o ${ZIP_TARGET} ${REPO_URI};
+  fi 
+  ${ECHO} "Unzipping Command line tools into your Downloads dir"
+  \unzip -o ${ZIP_TARGET} -d ${HOME}/Downloads;
+  ${ECHO} "Installing command line tools, please wait..."
+  sudo installer -pkg ${LOCAL_REPO_DIR}/clt/clt-ml.pkg -target ${OSX_GCC_TARGET};
+  ${ECHO} "CLT installed"
 
-if [[ ! -e ${ZIP_TARGET} ]]; then
-  $ECHO "Downloading Mac OSX Command Tools for Mountain Lion"
-  \curl -fsSL -o ${ZIP_TARGET} ${REPO_URI};
-fi 
-$ECHO "Unzipping Command line tools into your Downloads dir"
-unzip -o ${ZIP_TARGET} -d ${HOME}/Downloads;
-$ECHO "Installing command line tools, please wait..."
-sudo installer -pkg ${LOCAL_REPO_DIR}/clt/clt-ml.pkg -target ${OSX_GCC_TARGET};
-$ECHO "CLT installed"
+  # Get hold of Ruby
+  if command_exists "rvm"; then
+    ${ECHO} "Existing RVM found , moving on";
+  else
+    ${ECHO} "Getting hold of latest stable Ruby RVM";
+    \curl -L https://get.rvm.io | bash -s stable --autolibs=read-fail --auto-dotfiles;
+    source ~/.rvm/scripts/rvm;
 
-# Get hold of Ruby
-if command_exists "rvm"; then
-    $ECHO "Existing RVM found , moving on";
-else
-  $ECHO "Getting hold of latest stable Ruby RVM";
-  \curl -L https://get.rvm.io | bash -s stable --autolibs=read-fail --auto-dotfiles;
-  source ~/.rvm/scripts/rvm;
+    rvm install ruby;
+    type rvm | head -1;
+    ${ECHO} "RVM installed"
+  fi
 
-  rvm install ruby;
-  type rvm | head -1;
-fi
-
-# Get hold of Homebrew
-if command_exists "brew"; then
+  # Get hold of Homebrew
+  if command_exists "brew"; then
     $ECHO "Detected existing install of Homebrew, moving on...";
-else
+  else
     $ECHO "Getting Homebrew";
     ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)";
-fi;
+  fi;
 
-rvm autolibs homebrew;
+  rvm autolibs homebrew;
 
-$ECHO "Installing Heroku (gives you Git etc)";
-brew install --force heroku-toolbelt;
+  ${ECHO} "Installing Heroku (gives you Git etc)";
+  brew install heroku-toolbelt;
 
-$ECHO "Configuring git"
-git config --global core.autocrlf input
+  ${ECHO} "Configuring git"
+  git config --global core.autocrlf input
+} <&-
 if ! git config user.email; then
-  $ECHO "Please input your email address"
+  ${ECHO} "Please input your email address"
+  clrstdin
   while \read -r email; do
     git config --global user.email ${email};
   done;
 fi
 if ! git config user.name; then
-  $ECHO "Please input your first and last name";
+  ${ECHO} "Please input your first and last name";
+  clrstdin
   while \read -r first_name last_name; do
     git config --global user.name "${first_name} ${last_name}";
   done;
 fi;
 
-brew install --force wget libksba autoconf gmp4 gmp mpfr mpc automake;
+{
+  brew install wget libksba autoconf gmp4 gmp mpfr mpc automake;
 
-# Python
-$ECHO "Installing python"
-brew install --force python;
-pip install virtualenv virtualenvwrapper shyaml;
+  # Python
+  ${ECHO} "Installing python"
+  brew install python;
+  pip install virtualenv virtualenvwrapper shyaml;
 
-$ECHO "Installing ssh-copy-id";
-brew install --force ssh-copy-id
+  ${ECHO} "Installing ssh-copy-id";
+  brew install ssh-copy-id
 
-$ECHO "Getting zsh"
-brew install zsh zsh-completions;
-curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-echo "fpath=(/usr/local/share/zsh-completions \$fpath)" >> ${HOME}/.zshrc
+  ${ECHO} "Getting zsh"
+  brew install zsh zsh-completions;
+  curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+  echo "fpath=(/usr/local/share/zsh-completions \$fpath)" >> ${HOME}/.zshrc
 
 
-$ECHO "Installing slick git tools";
-brew install --force gist hub hubflow
-pip install git_sweep
+  ${ECHO} "Installing slick git tools";
+  brew install gist hub hubflow
+  pip install git_sweep
+} <&-
+
 if [ ! -f ${HOME}/.config/hub ]; then
   $ECHO "Please input your github username"
+  clrstdin
   while \read -r username; do
     mkdir ${HOME}/.config
     echo "eval \"\$(hub alias -s)\"" >> ${HOME}/.zshrc
     echo "github.com: \n  - user: ${username}" > ${HOME}/.config/hub
   done;
 fi
+{
+  ${ECHO} "Installing PHP..."
+  brew untap homebrew/dupes
+  brew tap homebrew/dupes
+  brew untap josegonzalez/homebrew-php
+  brew tap josegonzalez/homebrew-php
+  brew update
+  brew install --force php55
 
-$ECHO "Installing PHP..."
-brew untap homebrew/dupes
-brew tap homebrew/dupes
-brew untap josegonzalez/homebrew-php
-brew tap josegonzalez/homebrew-php
-brew update
-brew install --force php55
+  echo "export PATH=\"\$(brew --prefix php55)/bin:$PATH\"" >> ${HOME}/.zshrc
 
-echo "export PATH=\"\$(brew --prefix php55)/bin:$PATH\"" >> ${HOME}/.zshrc
+  source ${HOME}/.zshrc
 
-source ${HOME}/.zshrc
+  ${ECHO} "Getting composer"
+  \curl -sS https://getcomposer.org/installer | php
+  mv composer.phar ${COMPOSER_TARGET};
+  ${ECHO} "Composer installed globally at ${COMPOSER_TARGET}"
 
-$ECHO "Getting composer"
-\curl -sS https://getcomposer.org/installer | php
-mv composer.phar ${COMPOSER_TARGET};
-$ECHO "Composer installed globally at ${COMPOSER_TARGET}"
+  # Get hold of cask
+  $ECHO "Installing Cask"
+  brew untap phinze/homebrew-cask;
+  brew tap phinze/homebrew-cask;
+  brew update;
+  brew install brew-cask;
+  brew update;
 
-# Get hold of cask
-$ECHO "Installing Cask"
-brew untap phinze/homebrew-cask;
-brew tap phinze/homebrew-cask;
-brew update;
-brew install brew-cask;
-brew update;
+  #Iterate through the manifest and install everything in there.
 
-#Iterate through the manifest and install everything in there.
+  ${ECHO} "Reading through the Cask manifest"
+} <&-
 
-$ECHO "Reading through the Cask manifest"
 while read app; do
   $ECHO "Installing ${app}"
   brew cask install ${app};
